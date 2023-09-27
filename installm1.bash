c710ef5af3ebac9870a85e1fed16264f7df4922b
George Hill <georgehill010@gmail.com>
2023-09-27 12:16:40 +0100

    auto commit 2023-09-27 12:16:40

---
+++ installm1.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+rm -rf .venv
+pyenv local 3.8.17
+python -m venv .venv
+source .venv/bin/activate
+pip uninstall cython
+
+# might have to remove all these dependencies from setup.py or pyproject.toml
+OPENBLAS="$(brew --prefix openblas)" MACOSX_DEPLOYMENT_TARGET=13.3 python -m pip install cython --no-use-pep517
+
+OPENBLAS="$(brew --prefix openblas)" MACOSX_DEPLOYMENT_TARGET=13.3 python -m pip install pandas==1.0.5 --no-use-pep517
+
+OPENBLAS="$(brew --prefix openblas)" MACOSX_DEPLOYMENT_TARGET=13.3 python -m pip install "numpy>=1.19.4,<1.24.0" --no-use-pep517
+
+OPENBLAS="$(brew --prefix openblas)" MACOSX_DEPLOYMENT_TARGET=13.3 python -m pip install scipy --no-use-pep517
+
+OPENBLAS="$(brew --prefix openblas)" MACOSX_DEPLOYMENT_TARGET=13.3 python -m pip install statsmodels==0.12.2 --no-use-pep517
+
+
+OPENBLAS="$(brew --prefix openblas)" MACOSX_DEPLOYMENT_TARGET=13.3 python -m pip install PyYAML==5.4 --no-use-pep517
+
+
+pip install -r requirements.txt
+python setup.py develop
