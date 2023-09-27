50ab15f0afe087ea5198962e907498c42336cca8
George Hill <georgehill010@gmail.com>
2023-09-27 17:55:32 +0100

    auto commit 2023-09-27 17:55:32

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from pysystemtrade.sysinit.futures.seed_price_data_from_IB import seed_price_data_from_IB
 from pathlib import Path
 from pysystemtrade.sysinit.futures.rollcalendars_from_arcticprices_to_csv import build_and_write_roll_calendar
-# seed_price_data_from_IB("ETHANOL")
+seed_price_data_from_IB("ETHANOL")
 
-build_and_write_roll_calendar(
-    instrument_code="ETHANOL",
-    output_datapath=Path("~/ib_data_download").expanduser(),
-    )
+# build_and_write_roll_calendar(
+#     instrument_code="ETHANOL",
+#     output_datapath=Path("~/ib_data_download").expanduser(),
+#     )
